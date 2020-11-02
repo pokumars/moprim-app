210fe364d86387d6365862fc953eee628c8f75a2
zakariazaim17 <zakariaziouziou@gmail.com>
2020-11-03 00:49:18 +0200

    Adding navigation feature

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@
  *
  * @format
  */
+import 'react-native-gesture-handler';
 
 import {
   Colors,
@@ -24,55 +25,31 @@ import {
   View,
 } from 'react-native';
 
+//import Home from './Screens/HomeScreen';
+import Home from './Screens/HomeScreen';
+import Icon from 'react-native-vector-icons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {NavigationContainer} from '@react-navigation/native';
+import Profile from './Screens/Profile';
 import React from 'react';
+import Trips from './Screens/Mytrips';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+//const RootStack = createStackNavigator<RootStackParamList>();
+
+const Stack = createBottomTabNavigator();
 
 declare const global: {HermesInternal: null | {}};
 
 const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Trips" component={Trips} />
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
---
+++ Screens/Mytrips.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import {Text, View} from 'react-native';
+
+const Trips = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>My trips!!yes</Text>
+    </View>
+  );
+};
+
+export default Trips;
